58d8cb4c4277d433b8f45196d47a37701c97835f
Tair <gidrolaz.tr@gmail.com>
2026-03-03 11:36:27 +0700

    Добавлен сканер

---
+++ Compiler/Compiler/Scaner/Token.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerGUI.Scaner
+{
+    public class Token
+    {
+        public int Code
+        {
+            get 
+            {
+                return (int) Type;
+            }
+        }
+        public string Value { get; set; }
+        public int Line { get; set; }
+        public int StartPos { get; set; }
+        public int EndPos { get; set; }
+        public int AbsoluteIndex { get; set; }
+        public TokenType Type { get; set; }
+        public string TypeName => Type switch
+        {
+            TokenType.Id => "Идентификатор",
+            TokenType.ConstInt => "Целочисленная константа без знака",
+            TokenType.ConstString => "Строковая константа",
+            TokenType.ConstFloat => "Вещественная константа",
+            TokenType.ConstTrue => "Логическая константа (True)",
+            TokenType.ConstFalse => "Логическая константа (False)",
+            TokenType.WhiteSpace => "Разделитель (пробел)",
+            TokenType.Comma => "Разделитель (запятая)",
+            TokenType.OpenListDelimiter => "Разделитель (закрывающая скобка)",
+            TokenType.CloseListDelimiter => "Разделитель (открывающая скобка)",
+            TokenType.End_operator => "Конец оператора",
+            TokenType.Equal => "Оператор присваивания",
+            TokenType.Minus => "Арифметический оператор (-)",
+            TokenType.Plus => "Арифметический оператор (+)",
+            TokenType.Error => "Лексическая ошибка",
+            _ => "Неизвестная лексема"
+        };
+
+    }
+}
---
+++ Compiler/Compiler/Scaner/TokenType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilerGUI.Scaner
+{
+    public enum TokenType
+    {
+        // ИНДЕТИФИКАТОРЫ
+        Id = 10, 
+        // КОНСТАНТЫ
+        ConstInt = 20, 
+        ConstFloat = 21,
+        ConstString = 22,
+        ConstTrue = 23,
+        ConstFalse = 24,
+        // РАЗДЕЛИТЕЛИ
+        WhiteSpace = 30, 
+        Comma = 31,
+        OpenListDelimiter = 32,
+        CloseListDelimiter = 33,
+        End_operator = 34,
+        // МАТЕМАТИЧЕСКИЕ ЗНАКИ
+        Equal = 40, 
+        Plus = 41,
+        Minus = 42,
+        // ОШИБКИ
+        Error = 99
+    }
+}
